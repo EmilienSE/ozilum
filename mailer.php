49823297678846d80e36aae04d67b435f52cca6e
SECHILARIU Emilien <AS_SECEM@eolane.com>
2019-03-04 11:04:18 +0100

    beautified mail

--- mailer.php
+++ mailer.php
@@ -269,7 +269,7 @@ td[class="spechide"]
                                   <td align=\'center\' bgcolor=\'#1A54BA\' style=\'background:#1A54BA; padding:15px 18px;-webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;\'>
                                     <div class="contentEditableContainer contentTextEditable">
                                       <div class="contentEditable" align=\'center\'>';
-    $message_html .= '<a target=\'_blank\' href=\'mailto:'.$email.'\' class=\'link2\' style=\'color:#ffffff;\'>Lui envoyer un message</a>';
+    $message_html .= '<a target=\'_blank\' href=\'mailto:'.$email.'?subject=RE%3ADemande%20d%27informations%20sur%20OZILUM\' class=\'link2\' style=\'color:#ffffff;\'>Lui envoyer un message</a>';
     $message_html .= '</div>
                                     </div>
                                   </td>
