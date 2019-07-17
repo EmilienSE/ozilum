fae2067705d3ecd0f00ab20694193844a2078030
SECHILARIU Emilien <AS_SECEM@eolane.com>
2019-07-17 11:29:24 +0200

    aec illuminazione added

--- commons/projects.php
+++ commons/projects.php
@@ -44,7 +44,7 @@
                 <h4 class="text-white">SIDE Spa</h4>
                 <p class="mb-0 text-white-50" id="sideText">SIDE Spa est une société italienne qui produit des équipements techniques pour intérieur et extérieur. Fondée en 1982, SIDE a réalisé en 25 ans, une position de leader dans l'industrie de l'éclairage sur le marché italien et l'expansion dans l'international.</p>
                 <hr class="d-none d-lg-block mb-0 mr-0">
-                <a href="https://sidespa.it/it/testimonial" target="_blank" class="btn btn-primary float-lg-left">En savoir plus</a>
+                <a href="https://www.sidespa.it/it/testimonial" target="_blank" class="btn btn-primary float-lg-left">En savoir plus</a>
               </div>
             </div>
           </div>
@@ -83,6 +83,25 @@
                 <p class="mb-0 text-white-50" id="europhaneText">Concevoir, fabriquer et commercialiser des solutions d'éclairage public intelligentes et connectées. Avec 50 ans d'expérience et une qualité Made in France, Europhane offre une diversité de produits et une flexibilité de services.</p>
                 <hr class="d-none d-lg-block mb-0 mr-0">
                 <a href="https://europhane.com/" target="_blank" class="btn btn-primary float-lg-left">En savoir plus</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Project Five Row -->
+      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="img/aec.jpg" alt="Logo AEC Illuminazione" aria-describedby="aecText">
+        </div>
+        <div class="col-lg-6">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-left">
+                <h4 class="text-white">AEC Illuminazione</h4>
+                <p class="mb-0 text-white-50" id="aecText">Les produits AEC sont le fruit d'une étude pluridisciplinaire qui prend forme dans des espaces modernes réservés à la fabrication et dans des laboratoires de test sophistiqués, où la sécurité et les performances sont testées par du personnel qualifié en formation permanente.</p>
+                <hr class="d-none d-lg-block mb-0 ml-0">
+                <a href="http://www.aecilluminazione.fr/" target="_blank" class="btn btn-primary float-lg-right">En savoir plus</a>
               </div>
             </div>
           </div>
